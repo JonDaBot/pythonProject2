be9b9d0910a96f1b80c94429d6258f9f7a5a3550
JonDaBot <JonDaBot@gmail.com>
2023-05-25 18:52:59 -0400

    tetris is hard

--- Tetris Game App.py
+++ Tetris Game App.py
@@ -199,9 +199,14 @@ def draw_text_middle(text, size, color, surface):
     pass
 
 
-def draw_grid(surface, row, col):
-    pass
+def draw_grid(surface, grid):
+    sx = top_left_x
+    sy = top_left_y
 
+    for i in range(len(grid)):
+        pygame.draw.line(surface, (128,128,128), (sx, sy + i*block_size), (sx+play_width, sy+ i*block_size))
+        for j in range(len(grid[i])):
+            pygame.draw.line(surface, (128, 128, 128), (sx + j*block_size, sy),(sx + j*block_size, sy + play_height))
 
 def clear_rows(grid, locked):
 
@@ -209,16 +214,95 @@ def clear_rows(grid, locked):
  def draw_next_shape(shape, surface):
 
 
-  def draw_window(surface):
-   pass
+  def draw_window(surface, grid):
+   surface.fill((0, 0, 0))
+
+   pygame.font.init()
+   font = pygame.font.SysFont('comicsans',60)
+   label = font.render('Tetris', 1, (255, 255, 255))
+
+   surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
+
+   for i in range(len(grid)):
+       for j in range(len([i])):
+           pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y + i*block_size, block_size))
+
+  pygame.draw.rect(surface, (255,0,0), (top_left_x, top_left_y, play_width, play_height), 5)
+
+     draw_grid(surface, grid)
+     pygame.display.update()
+
+def main(win): # *
+
+    locked_positions = ()
+    grid = create_grid(locked_positions)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+    fall_speed = 0.27
+
+    while run:
+        grid = create_grid(locked_positions)
+        fall_time += clock.get_rawtime()
+        clock.tick()
+
+        if fall_time/1000 > fall_speed:
+            fall_time = 0
+            current_piece.y += 1
+            if not(valid_space(current_piece, grid)) and current_piece.y > 0:
+                current_piece.y -= 1
+                change_piece = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not(valid_space(current_piece,grid)):
+                        current_piece += 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not(valid_space(current_piece,grid)):
+                        current_piece -= 1
+                if event.key == pygame.K_DOWN:
+                    current_piece.x += 1
+                    if not(valid_space(current_piece,grid)):
+                        current_piece -= 1
+                if event.key == pygame.K_UP:
+                    current_piece.x += 1
+                    if not(valid_space(current_piece,grid)):
+                        current_piece -= 1
+
+        shape_pos = convert_shape_format(current_piece)
+
+        for i in range(len(shape_pos)):
+            x, y = shape_pos[1]
+            if y > -1:
+                grid[y][x] = current_piece.color
+
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0], pos[1])
+                locked_positions[p] = current_piece.color
+            current_piece = next_piece
+            next_piece = get_shape()
+            change_piece = False
+
+        draw_window(win, grid)
+
+        if check_lost(locked_positions):
+            run = False
+
+    pygame.display.quit()
 
 
-def main():
-    pass
-
-
-def main_menu():
-    pass
+def main_menu(win):   # *
+    main(win)
 
 
 main_menu()  # start game
